a199878956573b06c04001d254cc17019fdbe638
Atamert Rahma <atamertrahma@gmail.com>
2024-07-14 22:13:27 +0200

    types renamed

--- src/layer.rs
+++ src/layer.rs
@@ -1,8 +1,8 @@
 use ndarray::Array2;
 
-type Input = Array2<f64>;
-type Output = Array2<f64>;
+type Inputs = Array2<f64>;
+type Outputs = Array2<f64>;
 
 pub trait Layer {
-    fn forward(&self, inputs: Input) -> Output;
+    fn forward(&self, inputs: Inputs) -> Outputs;
 }
