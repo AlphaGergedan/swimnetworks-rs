e248e2a42fcf232757a4cb04f58ddfbcb717eeb6
Atamert Rahma <atamertrahma@gmail.com>
2024-07-15 12:43:56 +0200

    remove debug print

--- src/model/fit.rs
+++ src/model/fit.rs
@@ -12,23 +12,12 @@ impl Model {
         // now inputs to the linear 2 layer should be mapped with minimum error to the outputs
 
         let mut input_with_ones = concatenate![Axis(1), inputs, Array::ones((inputs.nrows(), 1))];
-        //println!("inputs dims: {:?}", inputs.dim());
-        //println!("outputs dims: {:?}", outputs.dim());
-        //println!("inputs with ones dims: {:?}", input_with_ones.dim());
         let params: Array2<f64> = input_with_ones.least_squares_in_place(&mut outputs.into_owned()).unwrap().solution;
-        //println!("A x = b <==> {} x = {}", input_with_ones, outputs);
-        //println!("FITTED WEIGHTS = {:?}", params);
 
-        //println!("-> params dim = {:?}", params.dim());
-
-        //println!("weights dim BEFORE = {:?}", self.linear2.weights.dim());
         let weights = params.slice(s![..-1, ..]).into_owned();
-        //println!("weights dim = {:?}", weights.dim());
         self.linear2.set_weights(weights);
 
-        //println!("biases dim BEFORE = {:?}", self.linear2.biases.dim());
         let biases = params.row(params.nrows() - 1).insert_axis(Axis(1)).into_owned();
-        //println!("biases dim = {:?}", biases.dim());
         self.linear2.set_biases(biases);
     }
 }
